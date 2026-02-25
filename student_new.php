91c6a55aff25cdc0fef5bcdc6d8b00d20be4f4cf
Samy0_o <97963987+SamyIOoOI@users.noreply.github.com>
2026-02-25 13:50:03 +0200

    Add files via upload

---
+++ student_new.php
@@ -0,0 +1,5 @@
+<?php
+    if ($_POST["user"]  != "") {$name = $_POST["user"]};
+    if ($_POST["Phone Number"] != "") {$tel = $_POST["Phone Number"]};
+    if ($_POST["Email Address"] != ""){$email = $_POST["Email Address"]};
+    ?>
